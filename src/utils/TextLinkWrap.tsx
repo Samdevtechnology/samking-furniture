d0f4c3eb9485efbf26d9aa13586335ecc7f60c61
Samdevtechnology <Samdevtechnology@gmail.com>
2023-05-27 19:23:01 +0100

    Updated - Styles Structure

--- src/utils/TextLinkWrap.tsx
+++ src/utils/TextLinkWrap.tsx
@@ -8,13 +8,12 @@ type props = {
 };
 
 const TextLinkWrap = ({ children, className, href, color }: props) => {
+  const classes = `w-max inline-block transition-all h-7 underline decoration-[0.5px] hover:decoration-1 underline-offset-4 hover:text-base ${
+    className ? className : ""
+  }`;
+
   return (
-    <Link
-      href={href}
-      className={`w-min inline-block transition-all h-7 underline decoration-[0.5px] hover:decoration-1 underline-offset-4 hover:text-base ${
-        className ? className : ""
-      }`}
-    >
+    <Link href={href} className={classes}>
       {children}
     </Link>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = withMT({
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/utils/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
