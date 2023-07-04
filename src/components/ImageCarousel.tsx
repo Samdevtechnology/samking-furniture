a5fdaff536f646a5b07ad29ace3e458623f79021
Samdevtechnology <Samdevtechnology@gmail.com>
2023-07-04 17:31:33 +0100

    Updated - Image Carousel

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -3,9 +3,9 @@
 import { Carousel, IconButton } from "@material-tailwind/react";
 import SubNav from "./SubNav";
 import Image from "next/image";
-import bg1 from "../assets/images/bg1.jpg";
-import bg2 from "../assets/images/bg2.jpg";
-import bg3 from "../assets/images/bg3.jpg";
+import bg1 from "@/assets/images/bg1.jpg";
+import bg2 from "@/assets/images/bg2.jpg";
+import bg3 from "@/assets/images/bg3.jpg";
 
 const nullElement = () => <div className="hidden"></div>;
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,11 @@
+import ImageCarousel from "@/components/ImageCarousel";
+import ProductCarousel from "@/components/ProductCarousel";
 import React from "react";
 
 export const Home = () => {
   return (
-    <>
-      <h1 className="text-9xl">Home</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus
-        excepturi reprehenderit facere eaque obcaecati aspernatur eveniet
-        doloribus hic laboriosam dicta. Mollitia neque dolore totam qui
-        voluptatem ab doloribus consectetur odio. Cupiditate, vitae debitis.
-        Temporibus, tenetur libero! Et aperiam, perferendis placeat nihil
-        architecto, autem error a neque excepturi atque veritatis commodi amet
-        expedita adipisci voluptas itaque possimus tempore. Quisquam, nostrum
-        possimus. Perspiciatis explicabo maxime quaerat totam nam ea
-        consequuntur quos doloremque eaque blanditiis officiis minima, itaque
-        unde sapiente, vero alias vel! Exercitationem in non dolor illo quasi
-        harum aut perspiciatis porro? Explicabo nulla exercitationem aliquid.
-        Corporis, perferendis. Consequatur voluptatem, perferendis sunt, odit
-        unde sit incidunt veritatis, porro ea possimus ex aut iusto. Cum nam
-        enim ipsa assumenda quae error ea facere! Rem similique, adipisci
-        obcaecati ullam inventore expedita eos recusandae pariatur laudantium
-        natus est, repellat maiores corporis excepturi molestiae tempore ad
-        blanditiis aliquid quidem, provident repellendus. Eum illo amet optio
-        soluta?
-      </p>
-    </>
+    <div>
+      <ImageCarousel />
+    </div>
   );
 };
