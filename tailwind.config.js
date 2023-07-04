c9a8659c9c6df1f8f08f258f5f0adeb8d303f60a
Samdevtechnology <Samdevtechnology@gmail.com>
2023-07-04 17:29:38 +0100

    Updated - Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
@@ -15,7 +17,9 @@ module.exports = withMT({
         secondary: "#C4D7ED",
         tertiary: "#375D81",
         container: "#E1E6FA",
+        offwhite: "#F5F5F3",
         textDark: "#333",
+        danger: colors.red[400],
       },
       fontFamily: {
         primary: ["var(--font-poppins)"],
