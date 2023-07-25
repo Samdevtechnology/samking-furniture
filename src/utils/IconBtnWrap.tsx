d3bc1a283b20d86c3d31b7a525527dc3ed7b9dda
Samdevtechnology <Samdevtechnology@gmail.com>
2023-07-25 19:29:51 +0100

    Updated - Utils Props

--- src/utils/IconBtnWrap.tsx
+++ src/utils/IconBtnWrap.tsx
@@ -1,14 +1,38 @@
+import Link from "next/link";
 import { ReactNode } from "react";
+
 type props = {
   children: ReactNode;
   className?: string;
   bg: string;
   size: number;
+  onClick?: React.MouseEventHandler;
+  href?: string;
 };
 
-const IconBtnWrap = ({ children, className, bg, size }: props) => {
+const IconBtnWrap = ({
+  children,
+  className,
+  bg,
+  size,
+  onClick,
+  href,
+}: props) => {
+  if (href) {
+    return (
+      <Link
+        href={href}
+        className={`w-${size} h-${size} flex just-cont hover:${bg}/10 active:${bg}/30 overflow-hidden ${
+          className ? className : ""
+        }`}
+      >
+        {children}
+      </Link>
+    );
+  }
   return (
     <button
+      onClick={onClick}
       className={`w-${size} h-${size} flex just-cont hover:${bg}/10 active:${bg}/30 overflow-hidden ${
         className ? className : ""
       }`}
--- src/utils/TextLinkWrap.tsx
+++ src/utils/TextLinkWrap.tsx
@@ -1,19 +1,38 @@
 import Link from "next/link";
 import { ReactNode } from "react";
+import clsx from "clsx";
+
 type props = {
   children: ReactNode;
   className?: string;
+  title?: string;
   href: string;
-  color?: string;
+  scale?: boolean;
+  pop?: boolean;
 };
 
-const TextLinkWrap = ({ children, className, href, color }: props) => {
-  const classes = `w-max inline-block transition-all h-7 underline decoration-[0.5px] hover:decoration-1 underline-offset-4 hover:text-base ${
-    className ? className : ""
-  }`;
-
+const TextLinkWrap = ({
+  children,
+  className,
+  href,
+  scale = true,
+  title,
+  pop,
+}: props) => {
   return (
-    <Link href={href} className={classes}>
+    <Link
+      title={title}
+      target={pop ? "_blank" : "_self"}
+      href={href}
+      className={clsx(
+        `w-max inline-block transition-all h-7 underline decoration-[0.5px] hover:decoration-1 underline-offset-4 ${
+          className ? className : ""
+        }`,
+        {
+          "hover:text-base": scale === true,
+        }
+      )}
+    >
       {children}
     </Link>
   );
