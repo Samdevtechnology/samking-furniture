0686eefe78788ac76677938bca6327ed8da0659d
Samdevtechnology <Samdevtechnology@gmail.com>
2023-08-17 17:31:55 +0100

    Updated - Button Config

--- src/utils/BUTTON_CONFIG.tsx
+++ src/utils/BUTTON_CONFIG.tsx
@@ -1,5 +1,16 @@
+type ColorVariantsProp = {
+  [key: string]: string;
+};
+
 const CUSTOM_BTN_CONFIG = (color: string = "primary") => {
-  return `bg-${color} shadow-${color}/20 hover:shadow-${color}/40`;
+  const colorVariants: ColorVariantsProp = {
+    facebook: "bg-facebook shadow-facebook/20 hover:shadow-facebook/40",
+    googleBlue: "bg-googleBlue shadow-googleBlue/20 hover:shadow-googleBlue/40",
+    primary: "bg-primary shadow-primary/20 hover:shadow-primary/40",
+  };
+
+  const className = `${colorVariants[color]}`;
+  return className;
 };
 
 export default CUSTOM_BTN_CONFIG;
