a1efc95414c2625ee22956f484bcbd043a0a2009
Samdevtechnology <Samdevtechnology@gmail.com>
2023-09-18 00:53:32 +0100

    Updated - Move Out Some logic

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,19 +1,8 @@
-import { FacebookSolid, Google } from "@/assets/icons/Socials";
-import Divider from "@/components/Divider";
 import SubNav from "@/components/SubNav";
 import Container from "@/utils/Container";
-import { Button } from "@/utils/MuiServerComponent";
-import TextLinkWrap from "@/utils/TextLinkWrap";
 import LoginForm from "./components/Form";
-import CUSTOM_BTN_CONFIG from "@/utils/BUTTON_CONFIG";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { redirect } from "next/navigation";
 
-const Page = async () => {
-  const data = await getServerSession(authOptions);
-  if (data) redirect("/customer/account");
-
+const Page = () => {
   return (
     <>
       <SubNav relative variant="secondary" />
@@ -25,40 +14,6 @@ const Page = async () => {
             </header>
             <div className="w-full">
               <LoginForm />
-              <Divider>OR</Divider>
-              <div>
-                <div className="flex justify-between items-center gap-x-8">
-                  <Button
-                    className={`rounded-md text-base flex just-cont ${CUSTOM_BTN_CONFIG(
-                      "facebook"
-                    )}`}
-                    fullWidth
-                  >
-                    <FacebookSolid
-                      color="white"
-                      className="inline w-5 h-5 mr-3"
-                    />
-                    Facebook
-                  </Button>
-                  <Button
-                    className={`rounded-md text-base flex just-cont ${CUSTOM_BTN_CONFIG(
-                      "googleBlue"
-                    )}`}
-                    fullWidth
-                  >
-                    <Google className="inline w-5 h-5 mr-3" />
-                    Google
-                  </Button>
-                </div>
-                <div>
-                  <h6 className="mt-4">
-                    I don&apos;t have an account yet ? &nbsp;
-                    <TextLinkWrap href="/register" className="font-medium">
-                      Create an Account
-                    </TextLinkWrap>
-                  </h6>
-                </div>
-              </div>
             </div>
           </main>
         </Container>
