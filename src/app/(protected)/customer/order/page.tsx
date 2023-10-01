345c746cb28f45a4a6e902d07e30984148718279
Samdevtechnology <Samdevtechnology@gmail.com>
2023-10-01 23:12:13 +0100

    Added - Order Page Styles (Static)

--- src/app/(protected)/customer/order/page.tsx
+++ src/app/(protected)/customer/order/page.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import Link from "next/link";
+import MainLayout from "../../components/Main";
 
 const page = () => {
-  return <div>Order Page</div>;
+  return (
+    <MainLayout title="Orders">
+      <header className=" pt-4 px-6">
+        <div className="flex rounded-t py-3 px-4 shadow">
+          <ul className="flex gap-x-8 text-lg text-primary">
+            <li>
+              <Link
+                href={"#"}
+                className="hover:decoration-1 hover:underline underline-offset-2"
+              >
+                Active Orders
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={"#"}
+                className="hover:decoration-1 hover:underline underline-offset-2"
+              >
+                Close Orders
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </header>
+    </MainLayout>
+  );
 };
 
 export default page;
