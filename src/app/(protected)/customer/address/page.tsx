f98dbcad424349aac3cfd4ecd41384667c4dcfda
Samdevtechnology <Samdevtechnology@gmail.com>
2023-10-01 23:10:20 +0100

    Added - Address Page Styles (Static)

--- src/app/(protected)/customer/address/page.tsx
+++ src/app/(protected)/customer/address/page.tsx
@@ -1,7 +1,53 @@
-import React from "react";
+import CardContainer from "../../components/CardContainer";
+import MainLayout from "../../components/Main";
+import { Card, Content, Footer, Header } from "../../components/Card";
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 const page = () => {
-  return <div>Address Page</div>;
+  return (
+    <MainLayout title="Address Book" link="Add New Address" href="#">
+      <CardContainer>
+        <Card>
+          <Content title="Tosin Mustapha">
+            <>
+              <p>56, Tafawa Balewa. Abuja-Wuye, Federal Capital Territory</p>
+              <p>+234 9080076547</p>
+            </>
+          </Content>
+          <Footer
+            sub={
+              <div className="text-sm">
+                <Link href={"#"}>Edit</Link>
+              </div>
+            }
+          >
+            Default Address
+          </Footer>
+        </Card>
+
+        <Card>
+          <Content title="Tosin Mustapha">
+            <>
+              <p>56, Tafawa Balewa. Abuja-Wuye, Federal Capital Territory</p>
+              <p>+234 9080076547</p>
+            </>
+          </Content>
+          <Footer
+            href="#"
+            sub={
+              <div className="flex gap-x-4 text-sm">
+                <Link href={"#"}>Edit</Link>
+                <Link href={"#"}>Delete</Link>
+              </div>
+            }
+          >
+            Set As Default
+          </Footer>
+        </Card>
+      </CardContainer>
+    </MainLayout>
+  );
 };
 
 export default page;
