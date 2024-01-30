62cfeb3aa88f90e063be986d2f525f760fbb8637
Samdevtechnology <Samdevtechnology@gmail.com>
2024-01-30 23:53:37 +0100

    Added - Product Page Layout

---
+++ src/app/product/[productId]/page.tsx
@@ -0,0 +1,20 @@
+import ImageTitle from "@/components/ImageTitle";
+
+const page = () => {
+  return (
+    <div>
+      <ImageTitle />
+      <section>
+        <div>
+          <div>{/* image */}</div>
+          <div>{/* product details */}</div>
+        </div>
+        <div>{/* more product details */}</div>
+        <div>{/* customer also view */}</div>
+        <div>{/* customer reviews */}</div>
+      </section>
+    </div>
+  );
+};
+
+export default page;
